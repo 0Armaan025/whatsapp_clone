69ae80826ebe61a85d3e781a1482595f6a44b8b9
Armaan <armaan33000@gmail.com>
2024-06-18 15:05:45 +0530

    fixed opactiy issue

--- lib/common/chat_inputfield.dart
+++ lib/common/chat_inputfield.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -15,6 +17,7 @@ class _ChatInputFieldState extends State<ChatInputField> {
   bool _isHoldingMic = false;
   final TextEditingController _messageController = TextEditingController();
   bool _hasText = false;
+  int _secondsElapsed = 0;
 
   @override
   void dispose() {
@@ -27,46 +30,50 @@ class _ChatInputFieldState extends State<ChatInputField> {
     final size = MediaQuery.of(context).size;
     return Column(
       children: [
-        Opacity(
-          opacity: _isHoldingMic ? 1.0 : 0.0,
-          child: Container(
-            width: double.infinity,
-            decoration: BoxDecoration(
-              color: Colors.black,
-              borderRadius: BorderRadius.circular(4),
-            ),
-            alignment: Alignment.center,
-            child: Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 10.0),
-                  child: CircleAvatar(
-                    backgroundColor: Colors.green,
-                    child: Text(
-                      "0:17",
-                      style: GoogleFonts.poppins(
-                          color: Colors.white, fontSize: 14),
-                    ),
-                  ),
+        _isHoldingMic
+            ? Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  
+                  color: _isHoldingMic ? Colors.black : Colors.transparent,
+                  borderRadius: BorderRadius.circular(4),
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 8.0),
-                  child: Container(
-                    child: Image(
-                      image: NetworkImage(
-                          "https://s3.ezgif.com/tmp/ezgif-3-3a5f822c0d.gif"),
+                alignment: Alignment.center,
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 10.0),
+                      child: CircleAvatar(
+                        radius: 30,
+                        backgroundColor: Colors.green,
+                        child: Text(
+                          _formatTimer(),
+                          style: GoogleFonts.poppins(
+                            color: Colors.white,
+                            fontSize: 14,
+                          ),
+                        ),
+                      ),
                     ),
-                  ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 8.0),
+                      child: Container(
+                        child: Image(
+                          image: NetworkImage(
+                              "https://s3.ezgif.com/tmp/ezgif-3-3a5f822c0d.gif"),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-              ],
-            ),
-            height: size.height * 0.07,
-            margin: EdgeInsets.only(
-                bottom: size.height * 0.02,
-                left: size.width * 0.02,
-                right: size.width * 0.02),
-          ),
-        ),
+                height: size.height * 0.07,
+                margin: EdgeInsets.only(
+                  bottom: size.height * 0.02,
+                  left: size.width * 0.02,
+                  right: size.width * 0.02,
+                ),
+              )
+            : Container(),
         Container(
           child: Row(
             children: [
@@ -77,7 +84,6 @@ class _ChatInputFieldState extends State<ChatInputField> {
                       _hasText = e.isNotEmpty;
                     });
                   },
-
                   controller: _messageController,
                   style: TextStyle(color: Colors.black), // Set text color
                   decoration: InputDecoration(
@@ -124,12 +130,15 @@ class _ChatInputFieldState extends State<ChatInputField> {
                 onTapDown: (details) {
                   setState(() {
                     _isHoldingMic = true;
+                    _secondsElapsed = 0;
                   });
+                  _startTimer();
                 },
                 onTapUp: (details) {
                   setState(() {
                     _isHoldingMic = false;
                   });
+                  _stopTimer(); // Stop timer
                 },
                 child: CircleAvatar(
                   radius: _isHoldingMic ? 32 : 28,
@@ -152,5 +161,36 @@ class _ChatInputFieldState extends State<ChatInputField> {
         ),
       ],
     );
+  }
+
+  String _formatTimer() {
+    if (!_isHoldingMic) {
+      return '';
+    } else {
+      int minutes = _secondsElapsed ~/ 60;
+      int seconds = _secondsElapsed % 60;
+      String minutesStr = minutes.toString().padLeft(2, '0');
+      String secondsStr = seconds.toString().padLeft(2, '0');
+      return '$minutesStr:$secondsStr';
+    }
+  }
+
+  void _startTimer() {
+    const oneSecond = Duration(seconds: 1);
+    Timer.periodic(oneSecond, (Timer timer) {
+      if (!_isHoldingMic) {
+        timer.cancel();
+      } else {
+        setState(() {
+          _secondsElapsed++;
+        });
+      }
+    });
+  }
+
+  void _stopTimer() {
+    setState(() {
+      _secondsElapsed = 0;
+    });
   }
 }
